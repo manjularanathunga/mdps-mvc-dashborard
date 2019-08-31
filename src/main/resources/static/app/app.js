1bd12ac18ffeb76d3c908049ecd73f090543e855
“Manjula <manjula.ranathunga.sl@gmail.com>
2019-08-31 12:57:36 +0800

    Temporary commit

--- src/main/resources/static/app/app.js
+++ src/main/resources/static/app/app.js
@@ -1,7 +1,21 @@
 var app = angular.module('myApp',['ngRoute'])
-    .run(['$rootScope','$location','$window','$http', function(){
+    .run(['$rootScope','$location','$window','$http', function($rootScope,$location,$window,$http){
         $rootScope.presentDate = new Date();
 
+        $rootScope.currentUserId    = $window.localStorage.getItem('mdbUserId') || false;
+        $rootScope.mdbRole          = $window.localStorage.getItem('mdbRole') || false;
+        $rootScope.authdata      = $window.localStorage.getItem('mdbAuthData') || false;
+
+        if($rootScope.currentUserId){
+            $http.defaults.headers.common[''] = 'Basic ' + $rootScope.authdata;
+        }
+
+        $rootScope.$on('$locationChangeStart', function (event, next, current) {
+           var restrictedPage = $.inArray($location.path(),['/login']) === -1;
+           if(restrictedPage && $rootScope.currentUserId == false){
+               $location.path('/login');
+           }
+        });
     }])
     .config(['$routeProvider', function ($routeProvider) {
         $routeProvider
@@ -9,7 +23,7 @@ var app = angular.module('myApp',['ngRoute'])
                 templateUrl : 'app/view/loginPage.html',
                 controller: 'LoginPageController'
             })
-            .when("/dashboard", {
+            .when("/", {
                 templateUrl : 'app/view/dashboard.html',
                 controller: 'DashboardPageController'
             })
